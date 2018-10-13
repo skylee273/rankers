c5f09cc7a2786439a8f12377c4a1cfe5556d73ca
이하늘 <skylee273@koreatech.kr>
2018-10-14 01:40:18 +0900

    NoteBook 수정작업

--- app/src/main/java/com/example/rankers/kakao/GlobalApplication.java
+++ app/src/main/java/com/example/rankers/kakao/GlobalApplication.java
@@ -20,6 +20,7 @@ public class GlobalApplication extends Application {
     // 깃 테스트 ㅇㅇ
     // 깃 허브 테스트 중이에영
     // 이해가 안되네..
+    // 노트북에서 수정함
     public static GlobalApplication getGlobalApplicationContext() {
         if (instance == null) {
             throw new IllegalStateException("This Application does not inherit com.kakao.GlobalApplication");
